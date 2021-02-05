1b57aa358edbac80551fc5ebcc91abc36485abde
张琼芳 <joanfen@xingren.com>
2021-02-05 18:29:09 +0800

    选择排序与插入排序代码

---
+++ src/com/joanfen/sort/InsertionSort.java
@@ -0,0 +1,29 @@
+package com.joanfen.sort;
+
+/**
+ * 插入排序
+ * 1. 1. 数组分为两部分，左边是排好序的，右边是未排序的
+ * 2. 每次取未排序的第一个元素与排好序的部分进行比较
+ * 3. 如果比排好序的元素小，则交换位置直至左边元素全部升序排列
+ * N^2/4 次交换，约 N^2/4 次比较
+ */
+public class InsertionSort extends Sort {
+    public void sort(Comparable[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j > 0; j--) {
+                if (less(a[j], a[j - 1])) {
+                    exch(a, j, j - 1);
+                } else break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        InsertionSort insertionSort = new InsertionSort();
+        Integer[] a = new Integer[]{9, 5, 10, 2, 3};
+        insertionSort.sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
---
+++ src/com/joanfen/sort/SelectionSort.java
@@ -0,0 +1,31 @@
+package com.joanfen.sort;
+
+/**
+ * 选择排序
+ * 1. 找到数组中最小的元素 A1
+ * 2. 将A1 与数组的第一个元素交换位置
+ * 3. 在剩下的数组中找到最小的元素A2
+ * 4. 与数组的第二个元素交换位置
+ * 5. 如此循环
+ * N 次交换，约 N^2/2 次比较
+ */
+public class SelectionSort extends Sort {
+    public void sort(Comparable[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (less(a[j], a[minIndex])) minIndex = j;
+            }
+            exch(a, i, minIndex);
+        }
+    }
+
+    public static void main(String[] args) {
+        SelectionSort selectionSort = new SelectionSort();
+        Integer[] a = new Integer[]{5, 9, 10, 2, 3};
+        selectionSort.sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
